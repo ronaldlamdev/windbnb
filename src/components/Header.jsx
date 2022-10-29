3e7f8aca1873d71fcbc9885c3f4d1d16b60cf2b1
milessteamaccount <ronaldlamdev@gmail.com>
2022-10-28 23:12:28 -0500

    set up html structure of search menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/images/logo.svg'
 import {AiOutlineSearch, AiOutlineClose} from 'react-icons/ai'
+import {MdLocationPin} from 'react-icons/md'
 
 const Header = () => {
+
+  const [menu, setMenu] = useState(false);
+
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+
   return (
     <header className='flex flex-col items-center gap-5'>
-      <img src={Logo} alt='logo' className='self-start'/>
+      <a className='self-start' href='/'>
+        <img src={Logo} alt='logo'/>
+      </a>
       <div className='flex shadow-[0px_1px_6px_0px_rgba(0,0,0,.1)] rounded-2xl justify-center items-center h-[55px] w-[297px]'>
         <div className='w-[138px] text-[#BDBDBD] font-["Mulish"] text-sm text-center'>Location</div>
         <div className='w-[106px] text-[#BDBDBD] font-["Mulish"] text-sm h-full flex justify-center items-center border-x-[1px]'>Add guests</div>
@@ -18,12 +28,47 @@ const Header = () => {
       <div className='absolute bottom-0 left-0 w-full h-[20vh] bg-black/60'></div>
       
       {/* Search menu */}
-      <div className='absolute top-0 left-0 w-full h-[80vh] bg-white'>
-        <div className='flex w-full justify-between items-center'>
-          <h1>Edit your search</h1>
-          <AiOutlineClose />
+      <div className='absolute top-0 left-0 w-full h-[80vh] bg-white shadow-md'>
+        <div className='flex flex-col items-center'>
+
+          {/* Heading and close icon */}
+          <div className='flex w-full justify-between items-center'>
+            <h1>Edit your search</h1>
+            <AiOutlineClose />
+          </div>
+
+          {/* Inputs */}
+          <div>
+            <div>
+              <div>Location</div>
+              <div>Location</div>
+            </div>
+            <div>
+              <div>Guests</div>
+              <div>Add Guests</div>
+            </div>
+
+              {/* Locations */}
+            <ul>
+              <li>
+                <MdLocationPin /> Helsinki, Finland
+              </li>
+              <li>
+                <MdLocationPin /> Turku, Finland
+              </li>
+              <li>
+                <MdLocationPin /> Oulu, Finland
+              </li>
+              <li>
+                <MdLocationPin /> Vaasa, Finland
+              </li>
+            </ul>
+          </div>
+
+          {/* Search button */}
+          <button><AiOutlineSearch /> Search</button>
+
         </div>
-        
       </div>
     </header>
   )
