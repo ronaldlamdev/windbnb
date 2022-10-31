f70d2d886997bb18ebc1c0d10e5813aac0d83270
milessteamaccount <ronaldlamdev@gmail.com>
2022-10-30 20:39:09 -0500

    created set functions that check if variable is a negative number and increment/decrement if it isn't

--- src/components/Guests.jsx
+++ src/components/Guests.jsx
@@ -6,6 +6,31 @@ const Guests = () => {
   const [adults, setAdults] = useState(0);
   const [children, setChildren] = useState(0);
 
+  
+  const PlusAdultCount = () => {
+    if (adults >= 0) {
+      setAdults(adults + 1);
+    };
+  };
+
+  const MinusAdultCount = () => {
+    if (adults > 0) {
+      setAdults(adults - 1);
+    };
+  };
+
+  const PlusChildrenCount = () => {
+    if (children >= 0) {
+      setChildren(children + 1);
+    };
+  };
+
+  const MinusChildrenCount = () => {
+    if (children > 0) {
+      setChildren(children - 1);
+    };
+  };
+
   return (
     <div className='px-6 py-9 font-["Mulish"]'>
 
@@ -14,9 +39,9 @@ const Guests = () => {
         <h1 className='text-[#333] font-bold'>Adults</h1>
         <span className='text-[#BDBDBD]'>Ages 13 or above</span>
         <div className='flex items-center mt-2'>
-          <button className=''><FiMinusSquare className='w-6 h-6' /></button>
+          <button onClick={MinusAdultCount} className=''><FiMinusSquare className='w-6 h-6' /></button>
           <span className='px-4'>{adults}</span>
-          <button className=''><FiPlusSquare className='w-6 h-6' /></button>
+          <button onClick={PlusAdultCount} className=''><FiPlusSquare className='w-6 h-6' /></button>
         </div>
       </div>
 
@@ -25,9 +50,9 @@ const Guests = () => {
         <h1 className='text-[#333] font-bold'>Children</h1>
         <span className='text-[#BDBDBD]'>Ages 2 - 12</span>
         <div className='flex items-center mt-2'>
-          <button className=''><FiMinusSquare className='w-6 h-6' /></button>
+          <button onClick={MinusChildrenCount} className=''><FiMinusSquare className='w-6 h-6' /></button>
           <span className='px-4'>{children}</span>
-          <button className=''><FiPlusSquare className='w-6 h-6' /></button>
+          <button onClick={PlusChildrenCount} className=''><FiPlusSquare className='w-6 h-6' /></button>
         </div>
       </div>
     </div>
