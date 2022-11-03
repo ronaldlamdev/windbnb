d740563286c4c2e2b6cdd3a12b90a417d0cb33e0
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-03 18:50:28 -0500

    setup html structure of card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {AiFillStar} from 'react-icons/ai'
 
-const Card = () => {
+const Card = ({title, photo, superHost, type, beds, rating}) => {
   return (
-    <div>Card</div>
+    <div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80" alt="room" />
+      </div>
+      <div>
+        <span>Super Host</span>
+        <span>Entire apartment</span>
+        <span>2 beds</span>
+        <span><AiFillStar /> 4.4 </span>
+      </div>
+      <h1>Stylist apartment in center of the city</h1>
+    </div>
   )
 }
 
