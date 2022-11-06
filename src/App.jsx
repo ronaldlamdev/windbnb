807fb193b9ff7b088cb31a24321c68b02f7a9a6f
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-06 01:45:18 -0500

    recreated the data context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from './components/Header'
 import Home from './components/Home'
+import DataContext from './context/DataContext'
+import { data } from './context/DataContext'
 
 const App = () => {
   return (
-    <div className='py-5'>
-      <Header />
-      <Home />
-    </div>
+    <DataContext.Provider value={data}>
+      <div className='py-5'>
+        <Header />
+        <Home />
+      </div>
+    </DataContext.Provider>
   )
 }
 
