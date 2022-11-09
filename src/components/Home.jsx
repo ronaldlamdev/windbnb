7ec0c3a822a8cbb99f9e70d3cca5012af14c2b5e
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-08 20:03:47 -0600

    created shorter custom classes for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
 
   return (
     <main className='mt-9 px-3'>
-      <div className='w-full flex justify-between items-center lg:px-24 xl:px-26 2xl:px-28 min-[1600px]:px-40'>
+      <div className='home-headings-container'>
         <h1 className='text-lg text-[#333] font-bold'>Stays in Finland</h1>
         <span className='font-medium text-[#4F4F4F] lg:text-lg'>12+ stays</span>
       </div>
-      <div className='py-5 grid grid-cols-1 sm:grid-cols-2 sm:gap-3 lg:grid-cols-3 lg:px-24'>
+      <div className='grid-section'>
         {rooms.map((room, index) => (
           <Card key={index} 
           title={room.title}
