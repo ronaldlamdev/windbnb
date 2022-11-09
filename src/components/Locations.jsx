dfb792feb6e54a6783fbace451907417a3cef05b
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-08 20:09:06 -0600

    created custom class for location option in location component

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -8,16 +8,16 @@ const Locations = () => {
 
   return (
     <ul className='px-6 py-9'>
-      <li onClick={() => setLocation('Helsinki, Finland')} className='flex w-fit items-center cursor-pointer text-[#4F4F4F] font-medium hover:text-[#828282] hover:underline hover:underline-offset-2 duration-500'>
+      <li onClick={() => setLocation('Helsinki, Finland')} className='location-option'>
         <MdLocationPin className='mr-2' /> Helsinki, Finland
       </li>
-      <li onClick={() => setLocation('Turku, Finland')} className='flex w-fit items-center pt-9 cursor-pointer text-[#4F4F4F] font-medium hover:text-[#828282] hover:underline hover:underline-offset-2 duration-500'>
+      <li onClick={() => setLocation('Turku, Finland')} className='location-option pt-9'>
         <MdLocationPin className='mr-2' /> Turku, Finland
       </li>
-      <li onClick={() => setLocation('Oulu, Finland')} className='flex w-fit items-center py-9 cursor-pointer text-[#4F4F4F] font-medium hover:text-[#828282] hover:underline hover:underline-offset-2 duration-500'>
+      <li onClick={() => setLocation('Oulu, Finland')} className='location-option py-9'>
         <MdLocationPin className='mr-2' /> Oulu, Finland
       </li>
-      <li onClick={() => setLocation('Vaasa, Finland')} className='flex w-fit items-center cursor-pointer text-[#4F4F4F] font-medium hover:text-[#828282] hover:underline hover:underline-offset-2 duration-500'>
+      <li onClick={() => setLocation('Vaasa, Finland')} className='location-option'>
         <MdLocationPin className='mr-2' /> Vaasa, Finland
       </li>
     </ul>
