90f0f7a02b3b675b4229901cb72b31309088e0ab
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-08 20:24:49 -0600

    created custom classes for card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,15 @@ import {AiFillStar} from 'react-icons/ai';
 
 const Card = ({title, photo, superHost, type, beds, rating}) => {
   return (
-    <div className='flex flex-col items-center text-center mx-auto w-full max-w-[457px] lg:max-w-[500px] cursor-pointer'>
+    <div className='card-container'>
       <div className='w-full h-[240px] lg:h-[300px]'>
         <img className='w-full h-full rounded-3xl' src={photo} alt={title} />
       </div>
-      <div className='mt-3 w-full flex justify-between items-center mx-auto'>
-        <span>{superHost} <span className='font-medium text-[#828282] text-sm lg:text-base'>{type} . {beds} beds</span></span>
+      <div className='card-container-headings'>
+        <span>{superHost} <span className='card-container-traits'>{type} . {beds} beds</span></span>
         <span className='flex items-center text-[#4F4F4F] lg:text-lg'><AiFillStar className='text-[#EB5757B8]'/> {rating}</span>
       </div>
-      <h1 className='mt-1 lg:mt-2 lg:text-lg mb-5 text-[#333] font-semibold self-start'>{title}</h1>
+      <h1 className='card-title'>{title}</h1>
     </div>
   )
 }
